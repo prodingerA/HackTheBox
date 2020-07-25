8d3e62083a5df2f6ef7fb1fa0146e93c3685d8cc
André Prodinger <prodinger.andre@gmail.com>
2020-07-26 00:59:32 +0200

    Solved
    + Solved and got the flag
    ? Little help trough https://sarthaksaini.com/2019/June/web-app/emdee-five-for-life.html

--- MD5 Challenge/ohyeah-scr1pt.py
+++ MD5 Challenge/ohyeah-scr1pt.py
@@ -3,16 +3,17 @@ from bs4 import BeautifulSoup
 import hashlib
 import re
 
+url = 'http://docker.hackthebox.eu:31846/'
 
-website = requests.get('http://docker.hackthebox.eu:31846/')
+r=requests.session()
+hesch = r.get(url)
+hesch = re.search("<h3 align='center'>+.*?</h3>",hesch.text)
+hesch = re.search("'>.*<",hesch[0])
+hesch = re.search("[^|'|>|<]...................",hesch[0])
+hesch=hashlib.md5(hesch[0].encode('utf-8')).hexdigest()
 
-cont = BeautifulSoup(website.content, 'html.parser')
-hesch = cont.find('h3').text
-heschInput = cont.find_all('form')
+data={'hash': hesch}
+hesch = r.post(url = url, data = data)
 
-print(hashlib.md5(hesch.encode('utf-8')).hexdigest())
-
-
-
-
+print(hesch.text)
 
